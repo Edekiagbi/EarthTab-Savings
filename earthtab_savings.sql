bc6cef2a84a914347a6e15286b10f15b0d320a54
Edekiagbi <153777807+Edekiagbi@users.noreply.github.com>
2025-02-24 20:19:32 +0100

    Add files via upload

---
+++ earthtab_savings.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 29, 2025 at 09:37 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `earthtab_savings`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance_info`
+--
+
+CREATE TABLE `attendance_info` (
+  `aten_id` int(20) NOT NULL,
+  `atn_user_id` int(20) NOT NULL,
+  `in_time` varchar(200) DEFAULT NULL,
+  `out_time` varchar(150) DEFAULT NULL,
+  `total_duration` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `attendance_info`
+--
+
+INSERT INTO `attendance_info` (`aten_id`, `atn_user_id`, `in_time`, `out_time`, `total_duration`) VALUES
+(42, 1, '22-03-2021 22:01:43', '19-01-2023 20:42:36', '22:40:53'),
+(43, 1, '19-01-2023 20:45:04', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_info`
+--
+
+CREATE TABLE `task_info` (
+  `task_id` int(50) NOT NULL,
+  `t_title` varchar(120) NOT NULL,
+  `t_description` text DEFAULT NULL,
+  `t_start_time` varchar(100) DEFAULT NULL,
+  `t_end_time` varchar(100) DEFAULT NULL,
+  `t_user_id` int(20) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0 COMMENT '0 = incomplete, 1 = In progress, 2 = complete'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `task_info`
+--
+
+INSERT INTO `task_info` (`task_id`, `t_title`, `t_description`, `t_start_time`, `t_end_time`, `t_user_id`, `status`) VALUES
+(29, '3000', '79733633', '97865', '2025-01-19 12:00', 29, 0),
+(28, 'earing', '3453', '7067025115', '2025-01-29 12:00', 28, 2),
+(30, '7,000,000', '0912347464', '199529', '2025-01-13 00:00', 30, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `user_id` int(20) NOT NULL,
+  `fullname` varchar(120) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `temp_password` varchar(100) DEFAULT NULL,
+  `user_role` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`user_id`, `fullname`, `username`, `email`, `password`, `temp_password`, `user_role`) VALUES
+(1, 'Admin', 'admin', 'admin@gmail.com', '0192023a7bbd73250516f069df18b500', NULL, 1),
+(30, 'Edeki', '199529', 'decclesiaste64@gmail.com', 'f076eb29d55ee03e6999ed8b6eb427a3', '', 2),
+(29, 'Agbi Abraham', 'Abraham', 'mudiaga.onojake@uniport.edu.ng', 'e4c3844b78c7532d193a4b8e4aef8570', '', 2),
+(28, 'Abraham', '7067025115', 'abrahamagbi49@gmail.com', '962c517fdcb6e51ddbafcfcf70d3c45d', '', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendance_info`
+--
+ALTER TABLE `attendance_info`
+  ADD PRIMARY KEY (`aten_id`);
+
+--
+-- Indexes for table `task_info`
+--
+ALTER TABLE `task_info`
+  ADD PRIMARY KEY (`task_id`);
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance_info`
+--
+ALTER TABLE `attendance_info`
+  MODIFY `aten_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
+-- AUTO_INCREMENT for table `task_info`
+--
+ALTER TABLE `task_info`
+  MODIFY `task_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `user_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
